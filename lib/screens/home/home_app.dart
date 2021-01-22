017216353d0098f7982d08e0efe372ace1af3d74
gajagajago <gajagajago@naver.com>
2021-01-22 18:05:16 +0900

    conditional render w/ auth token

--- lib/screens/home/home_app.dart
+++ lib/screens/home/home_app.dart
@@ -1,6 +1,27 @@
+import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_session/flutter_session.dart';
 
-class HomeApp extends StatelessWidget {
+class HomeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeAppState();
+  }
+}
+
+class _HomeAppState extends State<HomeApp> {
+  Future<String> authenticationToken;
+
+  @override
+  void initState() {
+    super.initState();
+    authenticationToken = initAuthToken();
+  }
+
+  Future<String> initAuthToken() async {
+    return await FlutterSession().get('authentication_token');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,25 +29,32 @@ class HomeApp extends StatelessWidget {
           title: Text('Home'),
         ),
       body: Container(
-        child: Column(
-          children: [
-            RaisedButton(
-              child: Text('Sign Up'),
-              onPressed: () => Navigator.pushNamed(context, '/sign_up'),
-            ),
-            RaisedButton(
-              child: Text('Sign In'),
-              onPressed: () => Navigator.pushNamed(context, '/sign_in'),
-            ),
-            RaisedButton(
-              child: Text('Quiz App'),
-              onPressed: () => Navigator.pushNamed(context, '/quiz'),
-            ),
-            RaisedButton(
-              child: Text('Expense App'),
-              onPressed: () => Navigator.pushNamed(context, '/expense'),
-            )
-          ],
+        child: ConditionalBuilder(
+          condition: authenticationToken != null,
+          builder: (context) => Column(
+            children: [
+              RaisedButton(
+                child: Text('Quiz App'),
+                onPressed: () => Navigator.pushNamed(context, '/quiz'),
+              ),
+              RaisedButton(
+                child: Text('Expense App'),
+                onPressed: () => Navigator.pushNamed(context, '/expense'),
+              )
+            ],
+          ),
+          fallback: (context) => Column(
+            children: [
+              RaisedButton(
+                child: Text('Sign Up'),
+                onPressed: () => Navigator.pushNamed(context, '/sign_up'),
+              ),
+              RaisedButton(
+                child: Text('Sign In'),
+                onPressed: () => Navigator.pushNamed(context, '/sign_in'),
+              ),
+            ],
+          ),
         ),
       ),
     );
