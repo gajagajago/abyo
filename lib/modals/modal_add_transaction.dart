77cfef04c27aee788c15cd40e288e44f0292d0c4
gajagajago <gajagajago@naver.com>
2021-01-29 00:56:15 +0900

    Modal add transaction toggle switch

--- lib/modals/modal_add_transaction.dart
+++ lib/modals/modal_add_transaction.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'dart:io' show Platform;
 import 'package:http/http.dart' as http;
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -19,6 +20,7 @@ class ModalAddTransaction extends StatefulWidget {
 
 class _ModalAddTransactionState extends State<ModalAddTransaction> {
   final _formKey = GlobalKey<FormBuilderState>();
+  bool amountPositive = true;
 
   void changeFormKey(String field, String data) {
     _formKey.currentState.fields[field].didChange(data);
@@ -27,112 +29,152 @@ class _ModalAddTransactionState extends State<ModalAddTransaction> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10)
-            )
-          ),
-        padding: EdgeInsets.all(10),
-        margin: MediaQuery.of(context).viewInsets,
-        child: Column(
-          children: [
-            FormBuilder(
-              key: _formKey,
-              child: Column(
-                children: [
-                  FormBuilderChoiceChip(
-                    name: 'category',
-                    decoration: InputDecoration(
-                      labelText: '자산 종류',
-                    ),
-                    options: [
-                      ...widget.assets.map((a) => FormBuilderFieldOption(
-                          value: a['id'],
-                          child: Text(HelperFunction()
-                              .assetCategory(a['category']))))
-                    ],
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10))),
+            padding: EdgeInsets.all(10),
+            margin: MediaQuery.of(context).viewInsets,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.15,
+                  child: FlutterSwitch(
+                      // width: 60.0,
+                      height: 30,
+                      toggleSize: 30.0,
+                      value: amountPositive,
+                      borderRadius: 30.0,
+                      padding: 2.0,
+                      activeToggleColor: Colors.red,
+                      inactiveToggleColor: Colors.blue,
+                      activeSwitchBorder: Border.all(
+                        color: Colors.red,
+                        width: 2.0,
+                      ),
+                      inactiveSwitchBorder: Border.all(
+                        color: Colors.blue,
+                        width: 2.0,
+                      ),
+                      activeColor: Colors.white,
+                      inactiveColor: Colors.white,
+                      activeIcon: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                      inactiveIcon: Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
+                      onToggle: (val) {
+                        setState(() {
+                          amountPositive = val;
+                        });
+                      }
                   ),
-                  FormBuilderTextField(
-                    name: 'title',
-                    decoration: InputDecoration(
-                      labelText: '항목',
-                    ),
-                    valueTransformer: (text) => text.trim(),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(context),
-                      FormBuilderValidators.max(context, 70),
-                    ]),
-                    keyboardType: TextInputType.text,
-                  ),
-                  FormBuilderTextField(
-                    name: 'amount',
-                    decoration: InputDecoration(
-                      labelText: '금액',
-                    ),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(context),
-                      FormBuilderValidators.numeric(context),
-                    ]),
-                    keyboardType: TextInputType.number,
-                  ),
-                  FormBuilderField(
-                    name: "time",
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(context)
-                    ]),
-                    builder: (FormFieldState<dynamic> field) {
-                      return InputDecorator(
+                ),
+                FormBuilder(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      FormBuilderChoiceChip(
+                        name: 'category',
                         decoration: InputDecoration(
-                          labelText: "일자",
-                          errorText: field.errorText,
+                          labelText: '자산 종류',
                         ),
-                        child: DatePickerTextButton(changeFormKey)
-                      );
-                    },
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      MaterialButton(
-                        child: Text("저장"),
-                        onPressed: () {
-                          if (_formKey.currentState.saveAndValidate()) {
-                            createTransaction(_formKey.currentState.value)
-                                .then((value) => {
-                              Navigator.of(context)
-                                  .popAndPushNamed('/asset')
-                            });
-                          }
+                        options: [
+                          ...widget.assets.map((a) => FormBuilderFieldOption(
+                              value: a['id'],
+                              child: Text(HelperFunction()
+                                  .assetCategory(a['category']))))
+                        ],
+                      ),
+                      FormBuilderTextField(
+                        name: 'title',
+                        decoration: InputDecoration(
+                          labelText: '항목',
+                        ),
+                        valueTransformer: (text) => text.trim(),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(context),
+                          FormBuilderValidators.max(context, 70),
+                        ]),
+                        keyboardType: TextInputType.text,
+                      ),
+                      FormBuilderTextField(
+                        name: 'amount',
+                        decoration: InputDecoration(
+                          labelText: '금액',
+                        ),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(context),
+                          FormBuilderValidators.numeric(context),
+                        ]),
+                        keyboardType: TextInputType.number,
+                      ),
+                      FormBuilderField(
+                        name: "time",
+                        validator: FormBuilderValidators.compose(
+                            [FormBuilderValidators.required(context)]),
+                        builder: (FormFieldState<dynamic> field) {
+                          return InputDecorator(
+                              decoration: InputDecoration(
+                                labelText: "일자",
+                                errorText: field.errorText,
+                              ),
+                              child: DatePickerTextButton(changeFormKey));
                         },
                       ),
-                      MaterialButton(
-                        child: Text("초기화"),
-                        onPressed: () {
-                          _formKey.currentState.reset();
-                        },
-                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          MaterialButton(
+                            child: Text("저장"),
+                            onPressed: () {
+                              if (_formKey.currentState.saveAndValidate()) {
+                                createTransaction(_formKey.currentState.value, amountPositive)
+                                    .then((value) => {
+                                          Navigator.of(context)
+                                              .popAndPushNamed('/asset')
+                                        });
+                              }
+                            },
+                          ),
+                          MaterialButton(
+                            child: Text("초기화"),
+                            onPressed: () {
+                              _formKey.currentState.reset();
+                            },
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             )
-          ],
         )
-      )
     );
   }
 }
 
-Future<bool> createTransaction(var params) async {
+Future<bool> createTransaction(var params, bool amountPositive) async {
+  Map<String, dynamic> paramsFormat = {
+    'category': params['category'],
+    'title': params['title'],
+    'amount': amountPositive ? params['amount'] : '-${params['amount']}',
+    'time': params['time']
+  };
+
   var url = Platform.isAndroid
-      ? 'http://10.0.2.2:3000/api/v1/assets/${params['category']}/transactions'
-      : 'http://127.0.0.1:3000/api/v1/assets/${params['category']}/transactions';
+      ? 'http://10.0.2.2:3000/api/v1/assets/${paramsFormat['category']}/transactions'
+      : 'http://127.0.0.1:3000/api/v1/assets/${paramsFormat['category']}/transactions';
 
   final response = await http.post(url,
-      headers: {'Content-Type': "application/json"}, body: jsonEncode(params));
+      headers: {'Content-Type': "application/json"}, body: jsonEncode(paramsFormat));
 
   return response.statusCode == 200;
 }
