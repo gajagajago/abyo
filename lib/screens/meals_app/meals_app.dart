fa5b94d972f2b5573b8a0c128782b2ae9b8b7a7c
gajagajago <gajagajago@naver.com>
2021-02-09 14:02:42 +0900

    meals app reset

--- lib/screens/meals_app/meals_app.dart
+++ lib/screens/meals_app/meals_app.dart
@@ -14,35 +14,6 @@ class MealsApp extends StatefulWidget {
 }
 
 class _MealsAppState extends State<MealsApp> {
-  List<dynamic> stocks;
-  List<dynamic> searchedResults;
-
-  _MealsAppState({this.stocks});
-
-  @override
-  void initState() {
-    super.initState();
-    fetchStocks();
-  }
-
-  Future<void> fetchStocks() async {
-    var url = Platform.isAndroid
-        ? 'http://10.0.2.2:3000/api/v1/stocks'
-        : 'http://127.0.0.1:3000/api/v1/stocks';
-
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      stocks = jsonDecode(response.body);
-    }
-  }
-
-  void _searchStock(String value) {
-    setState(() {
-      searchedResults = stocks.where((e) => e['name'].contains(value)).toList();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     double bodyHeight = MediaQuery.of(context).size.height - Scaffold.of(context).appBarMaxHeight - kBottomNavigationBarHeight - MediaQuery.of(context).padding.vertical;
@@ -50,28 +21,7 @@ class _MealsAppState extends State<MealsApp> {
     return Scaffold(
       body: Container(
         height: bodyHeight,
-        child: Center(
-          child: Column(
-            children: [
-              RaisedButton(onPressed: xmlToMap)
-
-            ],
-          )
-        ),
       ),
     );
-  }
-}
-
-Future<dynamic> xmlToMap() async {
-  String stockCode = "041190";
-  String url = "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=$stockCode";
-
-  final response = await http.get(url);
-
-  if(response.statusCode == 200) {
-    xml.XmlDocument data = xml.XmlDocument.parse(response.body);
-    String price = data.findAllElements('DailyStock').first.getAttribute('day_EndPrice');
-    print(price);
   }
 }
