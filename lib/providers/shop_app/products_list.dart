9fa83d618fe37a91a63e10f880b3d47f241af67d
gajagajago <gajagajago@naver.com>
2021-02-26 00:06:04 +0900

    fix fetch products

--- lib/providers/shop_app/products_list.dart
+++ lib/providers/shop_app/products_list.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'dart:io' show Platform;
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'product.dart';
+import '../../helpers/http_request.dart';
 
 class ProductsList with ChangeNotifier {
   List<Product> _products;
@@ -16,25 +15,21 @@ class ProductsList with ChangeNotifier {
 
   bool get favoriteFilter => _favoriteFilter;
   List<Product> get products => _favoriteFilter ? favoriteProducts : _products;
-  List<Product> get favoriteProducts => _products.where((p) => p.isFavorite).toList();
+  List<Product> get favoriteProducts =>
+      _products.where((p) => p.isFavorite).toList();
 
   Future fetchProductsList(String authToken) async {
     try {
       loading = true;
-      String url = Platform.isAndroid
-          ? 'http://10.0.2.2:3000/api/v1/products'
-          : 'http://127.0.0.1:3000/api/v1/products';
-
-      final response = await http.get(
-          url,
-          headers: {'Content-Type': "application/json", 'AUTH-TOKEN': authToken}
-        );
-
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonList = json.decode(response.body);
-        _products = jsonList.map((e) => Product.fromJson(e)).toList();
-        loading = false;
-      }
+      await HttpRequest()
+          .get(partialUrl: "products", authToken: authToken)
+          .then((response) {
+            if (response.statusCode == 200) {
+              final List<dynamic> jsonList = json.decode(response.body);
+              _products = jsonList.map((e) => Product.fromJson(e)).toList();
+              loading = false;
+            }
+          });
     } catch (e) {
       print(e);
     } finally {
