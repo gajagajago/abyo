7944e7900995b901c04a4b5b433ad1b927f8fe8d
gajagajago <gajagajago@naver.com>
2021-02-27 23:58:44 +0900

    temp

--- lib/screens/shop_app/product_item_screen.dart
+++ lib/screens/shop_app/product_item_screen.dart
@@ -22,15 +22,77 @@ class _ProductItemScreenState extends State<ProductItemScreen> {
     double bodyHeight = MediaQuery.of(context).size.height - Scaffold.of(context).appBarMaxHeight - MediaQuery.of(context).padding.vertical;
 
     return Container(
+      color: Colors.white,
       height: bodyHeight,
       child: Stack(
         children: [
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ProductItem(),
-              Container(
-                child: Text('수량: $productCount'),
-              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 52,
+                    maxWidth: 122,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.fromBorderSide(
+                            BorderSide(
+                              color: Colors.grey,
+                              // width: 1,
+                              style: BorderStyle.solid,
+                            )
+                        )
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 80,
+                          child: Center(
+                            child: Text(
+                              productCount.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            RaisedButton(
+                              color: Colors.white,
+                              child: Icon(Icons.keyboard_arrow_up),
+                              onPressed: () {
+                                setState(() {
+                                  productCount ++;
+                                });
+                              },
+                            ),
+                            RaisedButton(
+                              color: Colors.white,
+                              child: Container(
+                                  width: 40,
+                                  height: 25,
+                                  child: Icon(Icons.keyboard_arrow_down)
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  productCount ++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ),
+              )
             ],
           ),
           Positioned(
@@ -45,10 +107,10 @@ class _ProductItemScreenState extends State<ProductItemScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      color: Colors.white,
-                      height: 60,
+                      height: double.infinity,
                       width: (MediaQuery.of(context).size.width - 30) / 2,
                       child: RaisedButton(
+                        color: Colors.white,
                         child: Text(
                           '장바구니 담기',
                           style: TextStyle(color: Colors.blue),
@@ -63,10 +125,10 @@ class _ProductItemScreenState extends State<ProductItemScreen> {
                       ),
                     ),
                     Container(
-                      color: Colors.blue,
-                      height: 60,
+                      height: double.infinity,
                       width: (MediaQuery.of(context).size.width - 30) / 2,
                       child: RaisedButton(
+                        color: Colors.blue,
                         child: Text(
                           '구매하기',
                           style: TextStyle(color: Colors.white),
